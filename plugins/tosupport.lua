5c55a62c5c378d0db5679a5e351ac50bc935d6fa
ho3einkj <mkhalaj380@gmail.com>
2016-04-02 11:49:06 +0430

    Update tosupport.lua

--- plugins/tosupport.lua
+++ plugins/tosupport.lua
@@ -1,19 +1,34 @@
---- https://github.com/amirhmz
---- https://telegram.me/AmirDark
 do
 
-function run(msg, matches)
-  return " The Admin Id : \n @ho3einkj"
-  end
-return {
-  description = "shows support link", 
-  usage = "!tosupport : Return supports link",
-  patterns = {
-    "^([!/]tosupport)$",
-    "^(tosupport)$"
-  },
-  run = run
-}
+local function callback(extra, success, result)
+  vardump(success)
+  vardump(result)
 end
---- https://github.com/amirhmz
---- https://telegram.me/AmirDark
+
+local function run(msg, matches)
+  local user = 158990680
+
+  if matches[1] == "support" then
+    user = 'user#id'..user
+  end
+
+  -- The message must come from a chat group
+  if msg.to.type == 'chat' then
+    local chat = 'chat#id'..msg.to.id
+    chat_add_user(chat, user, callback, false)
+    return "سازنده ربات وارد گروه شد"
+  else 
+    return 'This isnt a chat group!'
+  end
+
+end
+
+return {
+  description = "support", 
+  patterns = {
+    "^[!/](support)$"
+  }, 
+  run = run 
+}
+
+end
